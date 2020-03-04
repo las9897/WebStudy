f25218ec85bad0bcec902baae41df67a18f7e4ae
JeJe <las9897@gmail.com>
2020-03-04 11:02:31 +0900

    spring init

---
+++ src/test/java/com/jeje/webstudy/WebstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeje.webstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebstudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
